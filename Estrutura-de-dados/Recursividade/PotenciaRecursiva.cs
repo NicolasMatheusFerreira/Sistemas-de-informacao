4077adf9af64ace4413f73c7095c072e2452015c
NicolasMatheusFerreira <71523671+NicolasMatheusFerreira@users.noreply.github.com>
2022-08-26 00:08:58 -0300

    Atualizando repositório

---
+++ Estrutura-de-dados/Recursividade/PotenciaRecursiva.cs
@@ -0,0 +1,20 @@
+﻿// Potenciação recursiva
+
+static double PotenciaRec(double a, int expo) {
+
+    if (expo==1)
+        return a;
+    else return a*PotenciaRec(a,--expo);
+}
+
+double a;
+int expo;
+
+Console.WriteLine("Potenciação recursiva.");
+Console.Write("Informe um valor para base: ");
+a = double.Parse(Console.ReadLine());
+Console.Write("Informe um valor para expoente: ");
+expo = int.Parse(Console.ReadLine());
+
+double result = PotenciaRec(a, expo);
+Console.WriteLine($"A potencia entre {a} e {expo} e igual a {result}");
