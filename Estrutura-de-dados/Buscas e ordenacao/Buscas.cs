eec4c2170944c77c15873feb69834cf29179ef95
NicolasMatheusFerreira <71523671+NicolasMatheusFerreira@users.noreply.github.com>
2022-09-20 23:10:16 -0300

    Atualizando repositório

--- Estrutura-de-dados/Buscas e ordenacao/Buscas.cs
+++ Estrutura-de-dados/Buscas e ordenacao/Buscas.cs
@@ -1,7 +1,32 @@
 using System;
 
 class Buscas {
+	static void Imprimir(int[] Vetor) {
+		for(int i = 0; i<Vetor.Length; i++)
+			Console.Write($"{Vetor[i]}, ");			
+		Console.WriteLine();		
+	}
+
+	// Ordenação
 	
+	static void SelectSort(int[] Vetor) {
+		
+		int menor, indice = 0, aux;
+
+		menor = Vetor[0];
+		for(int i = 0; i<Vetor.Length; i++) {			
+			for(int j = 0; j<Vetor.Length; j++) {
+				if (Vetor[j]<menor) {
+					menor = Vetor[j];
+					indice = j;
+				}
+			}
+			aux = Vetor[i];
+			Vetor[i] = Vetor[indice];
+			Vetor[indice] = aux;
+		}
+	}
+
 	static int BuscaSequencialRec(int alvo, int[] Vetor, int i) {
 	
 		if (i>=0) {
@@ -56,6 +81,12 @@ class Buscas {
 
 	static void Main(string[] args) {
 		
-		Console.WriteLine(BuscaSequencialRec(int.Parse(Console.ReadLine()), new int[] {2,2,4,7,11,21,23,30,41},8));
+//		Console.WriteLine(BuscaSequencialRec(int.Parse(Console.ReadLine()), new int[] {2,2,4,7,11,21,23,30,41},8));
+
+		int[] Valores = new int[] {25, 78, 101, 3, 65, 45, 2};
+
+		Imprimir(Valores);
+		SelectSort(Valores);
+		Imprimir(Valores);
 	}
 }
